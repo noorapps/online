600c6d82077145caf8519fb124ad54a34cda6cdb
noorapps <125349317+noorapps@users.noreply.github.com>
2023-02-16 14:44:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-config.brandingDataUrl = 'https://github.com/noorapps/online/blob/e4c8992db73da0a4ed27a879a395ebbddddcfd7f/branding.json'
+config.brandingDataUrl = 'https://panel.noorstudents.com/wp-content/uploads/2023/02/branding.json'

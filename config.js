96576963294a55ee0c8a8a3e15917171bb0ad1d4
noorapps <125349317+noorapps@users.noreply.github.com>
2023-02-16 14:42:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-config.brandingDataUrl = 'https://rawcdn.githack.com/maxired/myjitsi/master/branding.json'
+config.brandingDataUrl = 'https://github.com/noorapps/online/blob/e4c8992db73da0a4ed27a879a395ebbddddcfd7f/branding.json'
